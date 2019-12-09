d98c629fbb8cdecc1e5d38f923ee9cdb1ec63799
minhdc <minhdo@tamdongtam.vn>
2019-12-09 11:41:33 +0700

    change structure project to handle data loader

--- architect.py
+++ architect.py
@@ -9,6 +9,7 @@ class CNN:
         Set up CNN model
     """
     def __init__(self, input,
+                    label
                     trainable=True,
                     dropout=0.5,
                     node_fuly = params.NODE,
@@ -18,13 +19,33 @@ class CNN:
         self.dropout = dropout
         self.node_fuly = node_fuly
         self.embedding_size=1024
+        self.operation(input, label)
 
+    def operation(self, input, label):
+        # get output
+        output = self.model(input)
+        # get center loss
+        # loss = model_utils.center_loss(embedding=output, labels=label, num_classes=params.CLASSES)
+        self.loss = tf.losses.softmax_cross_entropy(onehot_labels=label, logits=output, scope='softmax_loss')
+        # get accuracy
+        # accuracy = tf.metrics.accuracy(labels=label, predictions=output, name='metric_acc')
+        correct_predict = tf.equal(tf.argmax(output, 1), tf.argmax(label, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_predict, tf.float32))
+        # get accuracy confusion matrix
+        confusion_matrix = tf.math.confusion_matrix(labels=tf.argmax(label, 1),
+                                                    predictions=tf.argmax(output, 1),
+                                                    num_classes=10,
+                                                    dtype=tf.dtypes.int32, name='confusion_matrix')
+        self.confusion_accuracy = model_utils.confusion_accuracy(confusion_matrix)
+        # compile model
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=params.LR, name='Adam_optimizer')
+        self.train_op = optimizer.minimize(self.loss, name='train_op')
 
-    def inference(self):
+    def model(self, __input):
         """
             CNN model
         """
-        _input = tf.reshape(self.input, [params.BATCH_SIZE, params.INPUT_SIZE, params.INPUT_SIZE, 1])
+        _input = tf.reshape(__input, [params.BATCH_SIZE, params.INPUT_SIZE, params.INPUT_SIZE, 1])
         net = tf.layers.conv2d(inputs=_input,
                                 filters=32,
                                 kernel_size=[5, 5],
--- train.py
+++ train.py
@@ -11,46 +11,25 @@ graph = tf.Graph()
 with graph.as_default():
     with tf.device('/gpu:0'):
         train_data, train_labels, validation_data, validation_labels = data_utils.train_test_split()
-        # define placeholder
-        input = tf.placeholder(shape=[params.BATCH_SIZE, params.INPUT_SIZE, params.INPUT_SIZE, 1], dtype=tf.float32)
-        label = tf.placeholder(shape=[params.BATCH_SIZE, params.CLASSES], dtype=tf.int32)
-        # get output
-        model = architect.CNN(input=input)
-        output = model.inference()
-        # get center loss
-        # loss = model_utils.center_loss(embedding=output, labels=label, num_classes=params.CLASSES)
-        loss = tf.losses.softmax_cross_entropy(onehot_labels=label, logits=output, scope='softmax_loss')
-        # get accuracy
-        # accuracy = tf.metrics.accuracy(labels=label, predictions=output, name='metric_acc')
-        correct_predict = tf.equal(tf.argmax(output, 1), tf.argmax(label, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_predict, tf.float32))
-        # get accuracy confusion matrix
-        confusion_matrix = tf.math.confusion_matrix(labels=tf.argmax(label, 1),
-                                                    predictions=tf.argmax(output, 1),
-                                                    num_classes=10,
-                                                    dtype=tf.dtypes.int32, name='confusion_matrix')
-        confusion_accuracy = model_utils.confusion_accuracy(confusion_matrix)
-        # compile model
-        optimizer = tf.train.AdamOptimizer(learning_rate=params.LR, name='Adam_optimizer')
-        train_op = optimizer.minimize(loss, name='train_op')
-
+        loader_data, num_images = data_utils.data_loader(train_data, train_labels)
+        batch_image, batch_label = loader_data.get_next()
+        model = architect.CNN(input=batch_image, label=batch_label)
 with graph.as_default():
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
-    loader_data, num_images = data_utils.data_loader(train_data, train_labels)
-    image_batch, label_batch = loader_data.get_next()
+    # image_batch, label_batch = loader_data.get_next()
     with tf.device('/gpu:0'):
         global_step = tf.global_variables_initializer()
         for epoch in range(1, params.EPOCHS + 1):
             print("[INFO] Epoch {}/{} - Batch Size {} - {} images".format(epoch, params.EPOCHS, params.BATCH_SIZE, num_images))
             iterator = 0
             while iterator < 32:
-                tensor_list = [global_step, loss, train_op, accuracy, confusion_accuracy]
+                tensor_list = [loader_data.initializer, global_step, model.loss, model.train_op, model.accuracy, model.confusion_accuracy]
                 feed_dict = {
-                        input: image_batch,
-                        label: label_batch
+                        input: train_data,
+                        label: train_labels
                 }
                 _step, _loss, _, acc, confusion_acc = sess.run(tensor_list, feed_dict=feed_dict)
                 print("STEP: ", _step)
