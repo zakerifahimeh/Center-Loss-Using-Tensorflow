32cfcb93416fc9a0d3364bdfbc4c4431a44150ed
minhdc <minhdo@tamdongtam.vn>
2019-12-10 11:57:28 +0700

    add tensorboard

--- train.py
+++ train.py
@@ -5,6 +5,8 @@ import data_utils
 import architect
 import model_utils
 
+if not os.path.exists(params.LOG_DIR):
+    os.mkdir(params.LOG_DIR)
 
 graph = tf.Graph()
 
@@ -13,12 +15,24 @@ with graph.as_default():
     # sess = tf.Session()
     config = tf.ConfigProto(allow_soft_placement=True)
     config.gpu_options.allow_growth = True
-    sess = tf.Session(config=config)
+    # making sure Tensorflow doesn't overflow the GPU
+    config.gpu_options.per_process_gpu_memory_fraction = 0.9
+    sess = tf.InteractiveSession(config=config)
     with tf.device("/device:GPU:0"):
         train_data, train_labels, validation_data, validation_labels = data_utils.train_test_split()
         # setup placeholder
         input = tf.placeholder(tf.float32, [None, params.INPUT_SIZE, params.INPUT_SIZE, params.NUM_CHANNELS])
         label = tf.placeholder(tf.int32, [None, params.CLASSES])
+        # Whenever you need to record the loss, feed the mean loss to this placeholder
+        tf_loss_ph = tf.placeholder(tf.float32,shape=None,name='loss_summary')
+        tf_accuracy_ph = tf.placeholder(tf.float32,shape=None, name='accuracy_summary')
+        tf_confusion_ph = tf.placeholder(tf.float32,shape=None, name='confusion_summary')
+        # Create a scalar summary object for the loss so it can be displayed
+        tf_loss_summary = tf.summary.scalar('loss', tf_loss_ph)
+        tf_accuracy_summary = tf.summary.scalar('accuracy', tf_accuracy_ph)
+        tf_confusion_summary = tf.summary.scalar('confusion acc', tf_confusion_ph)
+        # Merge all summaries together
+        performance_summaries = tf.summary.merge([tf_loss_summary, tf_accuracy_summary, tf_confusion_summary])
         #
         num_images = train_data.shape[0]
         num_val = validation_data.shape[0]
@@ -27,7 +41,8 @@ with graph.as_default():
         model = architect.CNN(batch_image, batch_label)
 with graph.as_default():
 
-    # image_batch, label_batch = loader_data.get_next()
+    # write graph
+    writer = tf.summary.FileWriter(params.LOG_DIR, sess.graph)
     with tf.device("/device:GPU:0"):
         sess.run(tf.global_variables_initializer())
         for epoch in range(1, params.EPOCHS + 1):
@@ -43,8 +58,13 @@ with graph.as_default():
             while train_step <= iterator + 1:
                 tensor_list = [model.loss, model.train_op, model.accuracy, model.confusion_accuracy]
                 _loss, _, acc, confusion_acc = sess.run(tensor_list)
-                print("{}/{} [INFO] Epoch {} - Accuracy: {:.4f} - Loss: {:.4f} - Confusion acc: {:.4f}".format(params.BATCH_SIZE*step,
+                print("{}/{} [INFO] Epoch {} - Accuracy: {:.4f} - Loss: {:.4f} - Confusion acc: {:.4f}".format(params.BATCH_SIZE*train_step,
                                                         num_images, epoch, acc, _loss, confusion_acc))
+                # Tensorboard
+                summ = sess.run(performance_summaries, feed_dict={tf_loss_ph: _loss,
+                                                                    tf_accuracy_ph: acc,
+                                                                    tf_confusion_ph: confusion_acc})
+                writer.add_summary(summ, train_step)
                 train_step += 1
             # set up for evaluate model
             iter = int(num_val/params.BATCH_SIZE)
